539f93ec4e1f1de3e8149d9a016d6ecad77c13d8
Matthew Hough <matthew.hough.2004@gmail.com>
2023-11-11 06:44:14 +0000

    Workshop.cs created.

---
+++ Vista.Web/Data/Workshop.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vista.Web.Data
+{
+    public class Workshop
+    {
+        public Workshop()
+        {
+        }
+
+        public Workshop(int workshopId, string name, DateTime dateAndTime)
+        {
+            WorkshopId = workshopId;
+            Name = name;
+            DateAndTime = dateAndTime;
+        }
+
+        public int WorkshopId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public string CategoryCode { get; set; } = null!;
+        public string BookingRef { get; set; } = null!;
+
+        // Placeholder for List of Staff (many side of one-to-many)
+    }
+}
