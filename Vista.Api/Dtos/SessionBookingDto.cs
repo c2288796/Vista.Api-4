e1f1dfce58f1d0f6c25aa0278d8491a677a92536
Matthew Hough <matthew.hough.2004@gmail.com>
2023-11-11 06:13:00 +0000

    SessionBookingDto.cs created.

---
+++ Vista.Api/Dtos/SessionBookingDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vista.Api.Dtos
+{
+    public class SessionBookingDto
+    {
+        public int SessionId { get; set; }
+
+        public int TrainerId { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime SessionDate { get; set; }
+
+        public string TrainerName { get; set; } = null!;
+
+        public string BookingReference { get; set; } = null!;
+    }
+}
