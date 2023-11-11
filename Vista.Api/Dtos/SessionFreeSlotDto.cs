a29003782a692edb9858ca164796e5f4a154854e
Matthew Hough <matthew.hough.2004@gmail.com>
2023-11-11 06:02:45 +0000

    Dtos folder created with SessionFreeSlotDto.cs added.

---
+++ Vista.Api/Dtos/SessionFreeSlotDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vista.Api.Dtos
+{
+    public class SessionFreeSlotDto
+    {
+        public int SessionId { get; set; }
+
+        public int TrainerId { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime SessionDate { get; set; }
+
+        public string TrainerName { get; set; } = null!;
+    }
+}
